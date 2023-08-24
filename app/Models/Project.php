382468ceea873c1a7d3678425a5928c917699c3e
andreathepa <andreadepa32@gmail.com>
2023-08-24 17:03:53 +0200

    seeding + category model and project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,11 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Support\Str;
+use App\Models\Category;
 
 class Project extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'content', 'slug', 'image'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 
     public static function generateSlug($title)
     {
